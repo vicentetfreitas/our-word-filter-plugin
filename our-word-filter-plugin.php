a65a7d439e5e03b3d953977870a3e0321ef61210
Vicente Teixeira Freitas <vicentefreitas@unimedce.com.br>
2024-04-22 15:27:45 -0300

    Custom Admin Menu IconCustom Admin Menu Icon

--- our-word-filter-plugin.php
+++ our-word-filter-plugin.php
@@ -19,14 +19,35 @@ class OurWordFilterPlugin
     }
     function ourMenu()
     {
-        add_menu_page('Word To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'), 'dashicons-smiley', 100);
+        // add_menu_page('Word To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'), plugin_dir_url(__FILE__) . 'custom.svg', 100);
+        $mainPageHook = add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'), 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+Cg==', 100);
         add_submenu_page('ourwordfilter', 'Word To Filter', 'Word List', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'));
         add_submenu_page('ourwordfilter', 'Word Filter Options', 'Options', 'manage_options', 'word-filter-options', array($this, 'optionsSubPage'));
+        add_action("load-{$mainPageHook}", array($this, 'mainPageAssets'));
     }
+
+    function mainPageAssets()
+    {
+        wp_enqueue_style('filterAdminCss', plugin_dir_url(__FILE__) . 'style.css');
+    }
+
     function wordFilterPage()
     { ?>
-
-Hello World
+<div class="wrap">
+    <h1>Word Filter</h1>
+    <form method="POST">
+        <label for="plugin_words_to_filter">
+            <p>
+                Enter a <strong>comma-separated</strong> list of words to filter from your site´s content.
+            </p>
+        </label>
+        <div class="word-filter__flex-container">
+            <textarea for="plugin_words_to_filter" id="plugin_words_to_filter" cols="30" rows="10"
+                class="word-filter__flex-container" placeholder="bad, mean, awful, horrible"></textarea>
+        </div>
+        <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+    </form>
+</div>
 <?php }
 
     function optionsSubPage()
